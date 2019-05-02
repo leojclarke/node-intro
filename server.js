533c77bb77e10d3c16d5f9d76fad8c3031eaa263
Leo Clarke <leojclarke@Leos-MacBook-Pro.local>
2019-05-02 17:36:07 +0200

    Add POST functionality

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const uuidv4 = require('uuid/v4');
 
 const app = express(); // function that creates new express server instance called 'app'
-
+app.use(express.json()); //Middleware
 let i = 0;
+
+const users = [
+  { id: uuidv4(), name: 'Joe', role: 'mechanic' },
+  { id: uuidv4(), name: 'Dr Who', role: 'time traveller' },
+  { id: uuidv4(), name: 'MacReady', role: 'scientist' },
+  { id: uuidv4(), name: 'Jack Reacher', role: 'detective' },
+  { id: uuidv4(), name: 'John McClane', role: 'time traveller' }
+];
 
 app.get('/', (request, response) => {
   // if a request ist received, repond with "Hello world"
-  response.send(`Hello world ${i++}`);
+  response.send(`Hello whole new world ${i++}`);
+});
+
+app.get('/users', (request, response) => {
+  response.json(users);
+});
+
+app.get('/users/:id', (request, response) => {
+  // :index is a placeholder for something
+  const { id } = request.params; // get user with id from the array
+  response.json(users.find(user => user.id === id)); // if id is equal to user id
+});
+
+app.post('/users', (request, response) => {
+  const user = { ...request.body, id: uuidv4() };
+  users.push(user);
+  console.log(users);
+  response.json(user);
 });
 
 app.listen(3000, err => {
