7e728c5d4edd07434d47b87bbc2ae2d4ff25ecb6
Leo Clarke <leojclarke@Leos-MacBook-Pro.local>
2019-05-02 16:05:01 +0200

    Add server that responds to get request

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const fs = require('fs');
+const express = require('express');
 
-fs.writeFile(__dirname + '/test.txt', 'Hello\nworld', err => {
-  if (err) {
-    console.log('There was an error');
-  } else {
-    console.log('File was written');
-  }
+const app = express(); // function that creates new express server instance called 'app'
+
+let i = 0;
+
+app.get('/', (request, response) => {
+  // if a request ist received, repond with "Hello world"
+  response.send(`Hello world ${i++}`);
+});
+
+app.listen(3000, err => {
+  // listen on port 3000 and console log if the server is ready
+  err ? console.log(err) : console.log('Server ready');
 });
